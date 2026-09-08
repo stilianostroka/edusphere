41839fd5873384f7a3f40ef207738ed78e3b35e0
Stilianos Troka <trokastilianos@gmail.com>
2026-09-09 01:08:41 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/edusphere/EdusphereApplication.java
@@ -0,0 +1,13 @@
+package com.edusphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdusphereApplication.class, args);
+	}
+
+}
